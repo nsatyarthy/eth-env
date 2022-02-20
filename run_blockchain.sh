d089ac1cb1626e594cc975cbd1abfdceee7b6316
nsatyarthy <6873481+nsatyarthy@users.noreply.github.com>
2022-02-20 09:13:29 +0800

    Update run_blockchain.sh

--- run_blockchain.sh
+++ run_blockchain.sh
@@ -6,8 +6,6 @@ chain_dir="${HOME}/${chain_name}"
 for i in $(seq 1 $n_nodes)
   do
     node_dir="${chain_dir}/node_0$i"
-    rm -rf "$node_dir"
-    mkdir -p "$node_dir"
-    echo "=> $geth --nodiscover --datadir $node_dir --mine --miner.threads 1 --port "33${i}1" --http.port "33${i}2" &"
+    echo "$geth --nodiscover --datadir $node_dir --mine --miner.threads 1 --port "33${i}1" --http.port "33${i}2" &"
     $geth --nodiscover --datadir $node_dir --mine --miner.threads 1 --port "33${i}1" --http.port "33${i}2" &
 done
